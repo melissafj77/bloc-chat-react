36a1c248201e7fc7ac6c88cd68afd72706afc408
Melissa Jacobini <melissa.jacobini@gmail.com>
2018-05-20 15:11:05 -0500

    Chat rooms added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as firebase from 'firebase';
 import './App.css';
+import RoomList  from './components/RoomList'
+
+ // Initialize Firebase
+ var config = {
+  apiKey: "AIzaSyDX9m5ck6Opy7Zwk3PfWCnpZt5L7Z_bj-Y",
+  authDomain: "bloc-chat-react-melissa.firebaseapp.com",
+  databaseURL: "https://bloc-chat-react-melissa.firebaseio.com",
+  projectId: "bloc-chat-react-melissa",
+  storageBucket: "bloc-chat-react-melissa.appspot.com",
+  messagingSenderId: "835566979935"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <RoomList firebase={firebase} />
       </div>
     );
   }
